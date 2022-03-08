7f3a2ac43907993065fca7c8aefacd221a7cf675
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2022-03-08 13:23:22 -0600

    Configuración Babel

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+const presets = [
+    [
+        "@babel/preset-env",
+        {
+            targets:{
+                edge: "17",
+                firefox: "60",
+                chrome: "67",
+                safari: "11.1",
+            },
+            corejs: "3.6.4",
+            useBuiltIns: "usage",
+        }
+    ]
+];
+
+const plugins = [
+    [
+      "babel-plugin-transform-not-strict",
+      {
+        removeAll: true,
+      },
+      "@babel/plugin-transform-runtime",
+    ],
+    
+];
+module.exports = {presets, plugins};
